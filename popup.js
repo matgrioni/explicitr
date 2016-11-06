88d75ab6d565abee5a72b791dbc9e5dd2c6041f0
Matias Grioni <matgrioni@gmail.com>
2016-11-06 10:50:59 -0500

    Lyrics now succesfully retrieved and small typo fix

--- popup.js
+++ popup.js
@@ -1,5 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var check = document.getElementById('check');
+  var artistText = document.getElementById('artist');
+  var songText = document.getElementById('song');
+  var checkButton = document.getElementById('check');
+
   check.addEventListener('click', function() {
+    var lyricsURL = metroLyricsURL(artist.value, song.value);
+    console.log(lyricsURL);
+
+    var http = new XMLHttpRequest();
+    http.responseType = "document";
+    http.open('GET', lyricsURL);
+    http.send();
+
+    var verses = Array.from(http.response.getElementsByClassName('verse'));
+    var lyrics = verses.reduce(function(acc, elt) {
+      return acc + elt.innerHtml;
+    }, "");
   });
 });
+
+function urlify(str) {
+  punct = /,;:!\.\?"~%\(\)/g
+  var newStr = str.toLowerCase().trim().replace(/\s/g, '-').replace(punct, '');
+  return newStr;
+}
+
+function metroLyricsURL(artist, song) {
+  return 'metrolyrics.com/'
+         + urlify(song) + '-lyrics-' + urlify(artist) + '.html';
+}
