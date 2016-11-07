427f394ae05a741b9c1801c78e8bbd6fbb504c03
Matias Grioni <matgrioni@gmail.com>
2016-11-07 10:50:19 -0500

    Pulled together loose strings. Now song can be identified as explicit.
    Fixed regex pattern to appropriately urlify artist and song names.
    Parse document tree properly to get lyrics from azlyrics.com.

--- popup.js
+++ popup.js
@@ -1,31 +1,51 @@
+var explicitWords = Object.freeze(['fuck', 'shit', 'nigga', 'nigger',
+                                   'cunt', 'motherfucker', 'ass',
+                                   'bitch', 'holy shit', 'whore']);
+
 document.addEventListener('DOMContentLoaded', function() {
   var artistText = document.getElementById('artist');
   var songText = document.getElementById('song');
   var checkButton = document.getElementById('check');
 
   check.addEventListener('click', function() {
-    var lyricsURL = metroLyricsURL(artist.value, song.value);
-    console.log(lyricsURL);
+    var lyricsURL = azLyricsURL(artist.value, song.value);
 
     var http = new XMLHttpRequest();
-    http.responseType = "document";
+    http.responseType = 'document';
+    http.addEventListener('load', function() {
+      var lyrics = lyricsDiv(http.response);
+
+      var explicit = explicitWords.reduce(function(acc, elt) {
+        return acc || lyrics.includes(elt);
+      }, false);
+
+      alert(explicit);
+    });
     http.open('GET', lyricsURL);
     http.send();
-
-    var verses = Array.from(http.response.getElementsByClassName('verse'));
-    var lyrics = verses.reduce(function(acc, elt) {
-      return acc + elt.innerHtml;
-    }, "");
   });
 });
 
+function lyricsDiv(dom) {
+  var contentDiv = dom.getElementsByClassName('col-xs-12 col-lg-8 text-center')[0];
+
+  var index = 8;
+  var child = contentDiv.children[index];
+  while (child.nodeName != "DIV" && !child.innerHTML) {
+    index++;
+    child = contentDiv.children[index];
+  }
+
+  return child.innerHTML;
+}
+
 function urlify(str) {
-  punct = /,;:!\.\?"~%\(\)/g
-  var newStr = str.toLowerCase().trim().replace(/\s/g, '-').replace(punct, '');
+  punct = /[,;:!\.\?"~%\(\)]/g
+  var newStr = str.toLowerCase().trim().replace(/\s/g, '').replace(punct, '');
   return newStr;
 }
 
-function metroLyricsURL(artist, song) {
-  return 'metrolyrics.com/'
-         + urlify(song) + '-lyrics-' + urlify(artist) + '.html';
+function azLyricsURL(artist, song) {
+  return 'http://azlyrics.com/lyrics/' +
+         urlify(artist) + '/' + urlify(song) + '.html';
 }
